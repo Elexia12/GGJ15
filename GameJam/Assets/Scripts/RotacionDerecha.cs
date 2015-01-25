fe4f4c58bb1d9bc237cf157cfe612f750bc7d92e
Elexia12 <xiannie.rivas@gmail.com>
2015-01-25 07:42:09 -0600

    Proyectos combinados

---
+++ GameJam/Assets/Scripts/RotacionDerecha.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotacionDerecha : MonoBehaviour {
+
+	public float vel;
+
+	// Use this for initialization
+	void Start () {
+		vel = 3.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+			transform.Rotate(Vector3.back * vel);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+			transform.Rotate(Vector3.forward * vel);
+		}
+	}
+}
---
+++ GameJam/Assets/Scripts/activarAnimacionConcreto.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class activarAnimacionConcreto : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+		Animator a = GetComponent<Animator>();
+		if (other.gameObject.tag == "LaMuerte") {
+			Debug.Log("Hubo colision");
+			a.SetBool("colision", false);
+		}
+	}
+		
+}
